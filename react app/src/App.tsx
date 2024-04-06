54ad1588422ba141b7a5d09709d3baaab0316049
adanmatute <adan.e.matute@gmail.com>
2024-04-06 14:38:23 -0700

    profile display/input gerneral format made

--- react app/src/App.tsx
+++ react app/src/App.tsx
@@ -2,14 +2,25 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 //import pages
 import Login from "./pages/Login";
+import Profile_Display from "./pages/Profile_Display"
+import Profile_Input from "./pages/Profile_Input"
 
 function App() {
+  const profileProps = {
+    name: "John Doe",
+    age: 30,
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at turpis vel libero tincidunt interdum.",
+    profilePictureUrl: "https://example.com/profile.jpg",
+    interests: ["Reading", "Cooking", "Traveling"]
+};
+
     return (
       <div className="App">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
-            {/* <Route path="signup" element={<SignUp />} /> */}
+            <Route path="/profile_display" element={<Profile_Display name={profileProps.name} age={profileProps.age} bio={profileProps.bio} profilePictureUrl={profileProps.profilePictureUrl} interests={profileProps.interests}/>}/>
+            <Route path="/input_page" element={<Profile_Input/>}/>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
