ada9e5c11b0178699ba4ca7b2dfae133d3604795
Arpan Savani <136035841+Arpan2207@users.noreply.github.com>
2024-04-06 13:53:32 -0700

    hbs to tsx

---
+++ react app/views/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <body>
+      <p>Welcome to the homepage of the App. Login to view posts and post videos.</p>
+    </body>
+  );
+};
+
+export default HomePage;
---
+++ react app/views/partials/navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface NavbarProps {
+  isLoggedIn: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ isLoggedIn }) => {
+  return (
+    <div className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item"><a href="/">Home</a></li>
+        <li className="navbar-item"><a href="/">name</a></li>
+        {/* Add more navbar items as needed */}
+        {isLoggedIn ? (
+          <li className="navbar-item">User Dropdown</li>
+        ) : (
+          <li className="navbar-item"><a href="/login">Login</a></li>
+        )}
+        <div className="navbar-marker"></div>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
