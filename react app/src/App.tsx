67a8e20e32118074302b277b0e41e1873b095dfe
Bruno Pastorelli <brunopastorelli12@gmail.com>
2024-04-06 19:06:21 -0700

    interests page made +-

--- react app/src/App.tsx
+++ react app/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Profile_Display from "./pages/Profile_Display"
 import Profile_Input from "./pages/Profile_Input"
+import TopicsOfInterest from "./pages/TopicsOfInterest";
 
 function App() {
   const profileProps = {
@@ -21,6 +22,7 @@ function App() {
           <Routes>  
             <Route path="/" element={<Login />} />
             <Route path="signup" element={<SignUp />} />
+            <Route path="/topics" element={<TopicsOfInterest />} />
             <Route path="/profile_display" element={<Profile_Display name={profileProps.name} age={profileProps.age} bio={profileProps.bio} profilePictureUrl={profileProps.profilePictureUrl} interests={profileProps.interests}/>}/>
             <Route path="/input_page" element={<Profile_Input/>}/>
             <Route path="*" element={<Navigate to="/" />} />
